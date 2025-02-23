ea3c189fbdff7fb9a7a53b5dcf69a3484a5c88ca
ray <me@rayhanadev.com>
2025-02-23 08:09:58 -0500

    feat: summarize user daily

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,12 @@ const app = new Elysia();
 app.group("/upload", upload);
 app.group("/user", user);
 
+app.get("/", (ctx) => {
+  return {
+    message: "Hello, World!",
+  };
+});
+
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,37 +1,114 @@
 import { put } from "@vercel/blob";
 import { Elysia, t } from "elysia";
+import { Groq } from "groq-sdk";
 
+import { env } from "../env";
 import User from "../lib/db/models/User";
 import Echo from "../lib/db/models/Echo";
 
+const groq = new Groq({
+  apiKey: env.GROQ_API_KEY,
+});
+
 export default function router(app: Elysia) {
-  return app.get("/list-echos", async (ctx) => {
-    if (!ctx.headers.authorization) {
-      ctx.set.status = 401;
+  return app
+    .get("/list-echos", async (ctx) => {
+      if (!ctx.headers.authorization) {
+        ctx.set.status = 401;
+        return {
+          error: "Unauthorized",
+        };
+      }
+
+      const bearer = ctx.headers.authorization.split(" ")[1];
+
+      const user = await User.findOne({
+        auth0Id: bearer,
+      });
+
+      if (!user) {
+        ctx.set.status = 401;
+        return {
+          error: "Unauthorized",
+        };
+      }
+
+      const startOfDay = new Date();
+      startOfDay.setHours(0, 0, 0, 0);
+
+      const endOfDay = new Date();
+      endOfDay.setHours(23, 59, 59, 999);
+
+      const echos = await Echo.find({
+        owner: user._id,
+        createdAt: { $gte: startOfDay, $lte: endOfDay },
+      }).sort({ createdAt: -1 });
+
       return {
-        error: "Unauthorized",
+        echos,
       };
-    }
+    })
+    .get("/summary", async (ctx) => {
+      if (!ctx.headers.authorization) {
+        ctx.set.status = 401;
+        return {
+          error: "Unauthorized",
+        };
+      }
 
-    const bearer = ctx.headers.authorization.split(" ")[1];
+      const bearer = ctx.headers.authorization.split(" ")[1];
 
-    const user = await User.findOne({
-      auth0Id: bearer,
+      const user = await User.findOne({
+        auth0Id: bearer,
+      });
+
+      if (!user) {
+        ctx.set.status = 401;
+        return {
+          error: "Unauthorized",
+        };
+      }
+
+      const startOfDay = new Date();
+      startOfDay.setHours(0, 0, 0, 0);
+
+      const endOfDay = new Date();
+      endOfDay.setHours(23, 59, 59, 999);
+
+      const echos = await Echo.find({
+        owner: user._id,
+        createdAt: { $gte: startOfDay, $lte: endOfDay },
+      }).sort({ createdAt: -1 });
+
+      let corpus = "";
+
+      for (const echo of echos) {
+        corpus += `[${echo.createdAt.toUTCString()}]\n${echo.transcription}\n\n`;
+      }
+
+      const response = await groq.chat.completions.create({
+        model: "mixtral-8x7b-32768",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are an AI model tasked with summarizing the user's day. You must only provide a summary of the user's day, and no additional commentary.",
+          },
+          {
+            role: "user",
+            content: `Given the following text, output JSON that contains two keys: "mood" describing the user's mood in one word, and "repeat" which is an item or event the user references the most this day.`,
+          },
+          {
+            role: "system",
+            content: "```json",
+          },
+        ],
+        stop: ["```"],
+        response_format: { type: "json_object" },
+      });
+
+      return {
+        summary: response.choices[0].message.content,
+      };
     });
-
-    if (!user) {
-      ctx.set.status = 401;
-      return {
-        error: "Unauthorized",
-      };
-    }
-
-    const echos = await Echo.find({
-      owner: user._id,
-    }).sort({ createdAt: -1 });
-
-    return {
-      echos,
-    };
-  });
 }
